7aaf07ce98b697b4463eddc9463bd71311d0c6b2
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-06-14 17:49:12 +0800

    [legacy-snake] first commit
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ legacy-snake/src/cljs/legacy_snake/app.cljs
@@ -0,0 +1,6 @@
+(ns legacy-snake.app)
+
+(defn init []
+  (let [c (.. js/document (createElement "DIV"))]
+    (aset c "innerHTML" "<p>i'm dynamically created</p>")
+    (.. js/document (getElementById "container") (appendChild c))))
