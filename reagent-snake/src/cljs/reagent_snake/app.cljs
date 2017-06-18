829855a21df8c5b38ee707ff1c197131e0554ef8
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-06-19 01:00:47 +0800

    [wip] add initial state
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- reagent-snake/src/cljs/reagent_snake/app.cljs
+++ reagent-snake/src/cljs/reagent_snake/app.cljs
@@ -1,6 +1,26 @@
 (ns reagent-snake.app
   (:require [reagent.core :as reagent :refer [atom]]))
 
+(def app-state
+  (atom {;; canvas object
+         ;; :canvas/element  (dom/getElement "canvas")
+         ;; :canvas/ctx  (-> (dom/getElement "canvas") (.getContext "2d"))
+         :canvas/background-color "white"    ; default canvas color (background)
+         :canvas/width  640
+         :canvas/height 480
+         ;; snake object
+         :snake/body '([0 0] [1 0] [2 0])    ; [x y]
+         :snake/direction [0 1]         ; default direction, see `keycode->direction`
+         :snake/width  32               ; 640 / 20
+         :snake/height 24               ; 480 / 20
+         :snake/border 2                ; border size
+         :snake/body-color "lime"       ; snake's body color
+         :snake/food nil                ; when `nil`, regenerate it
+         :snake/food-color "red"        ; the color of food
+         :snake/alive true              ; when `false`, stop game loop
+         }))
+
+
 (defn calling-component []
   [:div.container-narrow
    [:div.header
