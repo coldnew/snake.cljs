dfc5354468745027674079549c4d417ce29ec0a9
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-06-18 21:48:48 +0800

    [reagent-snake] Implement basic ui
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- reagent-snake/src/cljs/reagent_snake/app.cljs
+++ reagent-snake/src/cljs/reagent_snake/app.cljs
@@ -1,17 +1,15 @@
 (ns reagent-snake.app
   (:require [reagent.core :as reagent :refer [atom]]))
 
-(defn some-component []
-  [:div
-   [:h3 "I am a component!"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} " and red"]
-    " text."]])
-
 (defn calling-component []
-  [:div "Parent component"
-   [some-component]])
+  [:div.container-narrow
+   [:div.header
+    [:ul {:class "nav nav-pills pull-right"}
+     [:li [:a {:href "https://github.com/coldnew/snake.cljs"} "Source"]]
+     [:li [:a {:href "https://coldnew.github.io/snake.cljs/docs/uberdoc.html"} "Documentation"]]]
+    [:h3.muted "snake.cljs"]]
+   [:hr]
+   [:canvas {:id "canvas"}]])
 
 (defn init []
   (reagent/render-component [calling-component]
